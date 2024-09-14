83dac1bfd59a9c6092cdfac941b4a4d0c8ee2170
Prakash744 <bhattprakash043@gmail.com>
2024-09-14 22:25:10 +0545

    Add files via upload

---
+++ Practice set 14/script.js
@@ -0,0 +1,5 @@
+let newButton= document.createElement("button");
+newButton.innerText="click me";
+newButton.style.color="white";
+newButton.style.backgroundColor="red";
+document.querySelector("body").prepend(newButton); 
